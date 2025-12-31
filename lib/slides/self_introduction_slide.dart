a7fce6afb2d171255aa86b8c3c51263729fccf88
dicenull <dicenull.dev@gmail.com>
2025-12-31 16:34:25 +0900

    調整

--- lib/slides/self_introduction_slide.dart
+++ lib/slides/self_introduction_slide.dart
@@ -13,6 +13,10 @@ class SelfIntroductionSlide extends FlutterDeckSlideWidget {
   @override
   Widget build(BuildContext context) {
     final theme = context.slideTextTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+    final labelStyle = theme.body.copyWith(
+      color: colorScheme.onSurface.withOpacity(0.6),
+    );
     return FlutterDeckSlide.blank(
       builder: (context) => Center(
         child: Padding(
@@ -26,27 +30,43 @@ class SelfIntroductionSlide extends FlutterDeckSlideWidget {
                   text: TextSpan(
                     children: [
                       TextSpan(
-                        text: '自己紹介\n',
+                        text: '自己紹介',
                         style: theme.title,
                       ),
                       const TextSpan(text: '\n'),
                       TextSpan(
-                        text: '名前: 早坂太吾 / @dicenull\n',
+                        text: '名前:',
+                        style: labelStyle,
+                      ),
+                      TextSpan(
+                        text: ' 早坂太吾 / @dicenull',
                         style: theme.body,
                       ),
                       const TextSpan(text: '\n'),
                       TextSpan(
-                        text: '所属: 株式会社jig.jp\n',
+                        text: '所属:',
+                        style: labelStyle,
+                      ),
+                      TextSpan(
+                        text: ' 株式会社jig.jp',
                         style: theme.body,
                       ),
                       const TextSpan(text: '\n'),
                       TextSpan(
-                        text: 'Flutterとの関わり:\n2020年新卒入社からFlutterを使っています\n',
+                        text: 'Flutterとの関わり:',
+                        style: labelStyle,
+                      ),
+                      TextSpan(
+                        text: ' 2020年新卒入社から',
                         style: theme.body,
                       ),
                       const TextSpan(text: '\n'),
                       TextSpan(
-                        text: 'Burikaigiとの関わり:\n昨年はじめて参加、今年はじめて登壇します！',
+                        text: 'Burikaigiとの関わり:',
+                        style: labelStyle,
+                      ),
+                      TextSpan(
+                        text: ' 昨年初参加、今年初登壇します！',
                         style: theme.body,
                       ),
                     ],
